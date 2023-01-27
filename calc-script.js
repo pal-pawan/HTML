acefc1a0dafbfd59303033c2844793b8c10af1e2
Pawan <pawanpal2001@gmail.com>
2023-01-27 12:46:19 +0530

    added comments to calculator js file.

--- calc-script.js
+++ calc-script.js
@@ -1,11 +1,19 @@
+//constructed a string to hold values to be entered
 let string="";
+//selected all buttons that are in the buttons class
 let buttons = document.querySelectorAll('.Button');
+//to make an array of string that the user is putting in
+//and then adding eventListners to it
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (event)=>{
+        //if user clicks "=" then it will evaluate the
+        //given string to produce the results
         if(event.target.innerHTML == '='){
             string = eval(string)
             document.querySelector('input').value = string;
         }
+        //if the user clicks"CLEAR" then it will 
+        //clear the inputs given
         else if(event.target.innerHTML == 'CLEAR'){
             string = ""
             document.querySelector('input').value = string;
